6375d543ec6d1341d52dac0e15f6d926dec56199
jbishop7 <rbishop7@uwo.ca>
2021-02-23 14:18:32 -0500

    Added game files
    Added the game files to the repository. Followed the Git tutorial to add the gitignore but there are still a lot of files. This is likely due to the amount of sprites we have included in the game so far.

---
+++ Assets/__Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xPos = Input.GetAxis("Horizontal");
+        float yPos = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+        pos.x += xPos * speed * Time.deltaTime;
+        pos.y += yPos * speed * Time.deltaTime;
+        transform.position = pos;
+
+    }
+}
